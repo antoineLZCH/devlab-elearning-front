9c1431d45f7c25c8170c16cb8db128cb00bfdd06
paul santamaria <pauliix.santamaria@gmail.com>
2019-12-05 17:17:33 +0100

    v1

--- components/CardComponent.vue
+++ components/CardComponent.vue
@@ -2,7 +2,7 @@
   <div class="flex justify-center items-center flex-col border-solid border-4 border-gray-600 w-1/3">
     <div class="flex-1 flex justify-between w-full">
       <p class="title p-2">Title</p>
-      <slot class="p-2" name="action"></slot>
+      <slot name="action"></slot>
     </div>
     <div class="flex-1 bg bg-gray-700 w-32 border-solid border-2 border-gray-600 w-auto m-1">
       <slot name="content"></slot>
