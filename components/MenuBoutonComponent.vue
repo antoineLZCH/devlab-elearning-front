bfe4c1ec36c5b2463c700d63a09306279933b6fc
paul santamaria <pauliix.santamaria@gmail.com>
2019-12-09 22:53:19 +0100

    menuBouton added

---
+++ components/MenuBoutonComponent.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+   <button :style="{color: activecolor}" class="block m-auto w-full" :class="{rounded:rounded}"  >
+       <slot name="icon" class="p-3"></slot>
+   </button>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+    props: {
+        rounded: {rounded:Boolean, default: 'square' },
+        icon:String, 
+        activecolor:String
+    },
+    data() {
+        return {
+         activecolor: 'red'   
+        }
+    },
+
+}
+</script>
+
+<style>
+.square{
+    border-radius: unset
+}
+.rounded{
+    border-radius: 20px
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,34 @@
 <template>
   <CardComponent card='titre de la card'>
-    <div slot="action">
-      <p class="p-3">{{action}}</p>
+    <div slot="action" class="flex">
+      <span :style="{color:tqt}" class="p-3">{{action}}</span>
     </div>
     <div slot="content">
       <img src="../assets/folder.svg" alt="icon_folder"> 
+    <MenuBoutonComponent>
+        <span slot="icon">{{icon}}</span>
+    </MenuBoutonComponent>
     </div>
-   
+    <br>
+    
   </CardComponent>
+
 </template>
 
 <script>
 import CardComponent from '~/components/CardComponent.vue'
+import MenuBoutonComponent from '~/components/MenuBoutonComponent.vue'
 
 export default {
   components: {
-    CardComponent
+    CardComponent,
+    MenuBoutonComponent
   },
   data() {
     return {
       action: 'dynamique action',
+      rounded: true,
+      icon: 'button'
     }
   },
 }
