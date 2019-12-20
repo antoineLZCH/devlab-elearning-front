36b272b91243f78716fff8301262fcab8eaa60e1
Antoine LZCH <antoine@agence-osez.com>
2019-12-20 12:25:21 +0100

    branch lzch

---
+++ components/CardComponent.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="card rounded shadow-lg">
+        <img src="http://lorempixel.com/400/200/" class="w-full">
+        <div class="px-6 py-4">
+            <h3 class="font-bold text-xl mb-2">{{ course.title }}</h3>
+            <p class="text-gray-700 text-base">
+                 {{ course.description }}
+            </p>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue, {PropOptions} from 'vue';
+import ICourse from '@/interfaces/ICourse';
+
+export default Vue.extend({
+    name: 'Card',
+
+    props: {
+        course: {
+          type: Object,
+          required: true
+        } as PropOptions<ICourse>
+    },
+
+    data() {
+        return {
+
+        }
+    }
+})
+
+</script>
+
+<style lang="scss">
+.card {
+    max-width: 400px;
+    img {
+        object-fit: cover;
+    }
+}
+</style>
---
+++ interfaces/ICourse.ts
@@ -0,0 +1,8 @@
+
+export default interface ICourse {
+  name: String,
+  description: String,
+  numberOfLessons: Number,
+  numberOfTasks: Number,
+  duration: String
+}
