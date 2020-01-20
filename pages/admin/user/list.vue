725d87cfe9fab8f56a5ff68d8a84b973a9996861
mmolano <miike.molano@gmail.com>
2020-01-20 20:20:58 +0100

    working on sorting filter table

--- pages/admin/user/list.vue
+++ pages/admin/user/list.vue
@@ -3,31 +3,43 @@
   <div class="head">
     <h1>Liste des utilisateurs</h1>
   </div>
-  <div>
+  <div class="filters">
     <nuxt-link class="Add-stylus" to="NewUser">New User</nuxt-link>
+    <section class="bg-indigo-dark h-50 p-2">
+              <div class="container mx-auto">
+                <input v-model="search" class="w-full h-16 px-3 rounded focus:outline-none focus:shadow-outline text-xl px-8 shadow-lg" type="search" placeholder="Search User...">
+          </div>
+      </section>
   </div>
   <table class="table">
     <thead>
-      <tr>
+      <!-- <tr>
         <th scope="col">Nom Prénom</th>
         <th scope="col">Équipe</th>
         <th scope="col">Niveau</th>
         <th scope="col">Nombre de formations</th>
         <th scope="col">Actions</th>
+      </tr> -->
+      <tr>
+        <th scope="col" v-for="column in columns" :key="column">
+            <a href="#" @click="sortBy(column)">
+              {{ column }}
+            </a>
+        </th>
       </tr>
     </thead>
     <tbody>
-      <tr v-for="users in pageOfItems" :key="users.id">
+      <tr v-for="users in userSearch" :key="users.id" >
         <td><img src="" alt="">{{ users.first_name }}</td>
         <td>{{ users.team }}</td>
         <td>{{ users.level }}</td>
         <td>{{ users.formation_nbr }}</td>
-        <td><button class="mainbutton">Voir</button> | <button class="deletebutton">Delete</button></td>
+        <td><button class="mainbutton">Edit</button> | <button class="deletebutton">Delete</button></td>
       </tr>
     </tbody>
   </table>
 
-    <div class="card-footer pb-0 pt-3"><jw-pagination :items="users" @changePage="onChangePage"></jw-pagination></div>
+    <div class="pagination_container"><jw-pagination :pageSize="5" :maxPages="5" :items="users" @changePage="onChangePage" :labels="customLabels"></jw-pagination></div>
    
   </section>
 </template>
@@ -38,25 +50,37 @@ import JwPagination from 'jw-vue-pagination';
 
 Vue.component('jw-pagination', JwPagination);
 
+const customLabels = {
+    first: '<<',
+    last: '>>',
+    previous: '<',
+    next: '>'
+};
+
 export default {
     data() {
         return {
-            pageOfItems: [],
-            users: [
+          sortkey: 'name',
+          reverse: false,
+          columns: ['Nom Prénom', 'Équipe', 'Niveau', 'Nombre de formations', 'Actions'],
+          search: '',
+          customLabels,
+          pageOfItems: [],
+          users: [
           { id: 1, first_name: 'Fred Flintstone', team: '3', level: '2', formation_nbr: '5' },
-          { id: 2, first_name: 'Wilma Flintstone', team: '3', level: '2', formation_nbr: '5' },
-          { id: 3, first_name: 'Barney Rubble', team: '3', level: '2', formation_nbr: '5'  },
-          { id: 4, first_name: 'Betty Rubble', team: '3', level: '2', formation_nbr: '5'  },
-          { id: 5, first_name: 'Pebbles Flintstone', team: '3', level: '2', formation_nbr: '5'  },
+          { id: 2, first_name: 'Wilma Flintstone', team: '3', level: '1', formation_nbr: '5' },
+          { id: 3, first_name: 'Barney Rubble', team: '3', level: '1', formation_nbr: '5'  },
+          { id: 4, first_name: 'Betty Rubble', team: '2', level: '2', formation_nbr: '5'  },
+          { id: 5, first_name: 'Pebbles Flintstone', team: '3', level: '4', formation_nbr: '5'  },
           { id: 6, first_name: 'Bamm Bamm Rubble', team: '3', level: '2', formation_nbr: '5'  },
-          { id: 7, first_name: 'The Great Gazzoo', team: '3', level: '2', formation_nbr: '5'  },
-          { id: 8, first_name: 'Rockhead Slate', team: '3', level: '2', formation_nbr: '5'  },
+          { id: 7, first_name: 'The Great Gazzoo', team: '3', level: '5', formation_nbr: '5'  },
+          { id: 8, first_name: 'Rockhead Slate', team: '4', level: '2', formation_nbr: '5'  },
           { id: 9, first_name: 'Pearl Slaghoople', team: '3', level: '2', formation_nbr: '5'  },
           { id: 10, first_name: 'Pearl Slaghoople', team: '3', level: '2', formation_nbr: '5'  },
-          { id: 11, first_name: 'Pearl Slaghoople', team: '3', level: '2', formation_nbr: '5'  },
-          { id: 12, first_name: 'Pearl Slaghoople', team: '3', level: '2', formation_nbr: '5'  },
+          { id: 11, first_name: 'Pearl Slaghoople', team: '3', level: '1', formation_nbr: '5'  },
+          { id: 12, first_name: 'Pearl Slaghoople', team: '5', level: '2', formation_nbr: '5'  },
           { id: 13, first_name: 'Pearl Slaghoople', team: '3', level: '2', formation_nbr: '5'  },
-          { id: 14, first_name: 'Pearl Slaghoople', team: '3', level: '2', formation_nbr: '5'  },
+          { id: 14, first_name: 'Pearl Slaghoople', team: '3', level: '8', formation_nbr: '5'  },
           { id: 15, first_name: 'Pearl Slaghoople', team: '3', level: '2', formation_nbr: '5'  },
         ]
         };
@@ -65,8 +89,19 @@ export default {
         onChangePage(pageOfItems) {
             // update page of items
             this.pageOfItems = pageOfItems;
+        },
+        sortBy: function(sortKey) {
+          this.reverse = (this.sortKey == sortKey) ? ! this.reverse : false;
+          this.sortKey = sortKey;
         }
+    },
+    computed: {
+    userSearch() {
+      return this.pageOfItems.filter(users => {
+        return users.first_name.toLowerCase().includes(this.search.toLowerCase())
+      })
     }
+  }
 };
 </script>
 
@@ -74,7 +109,7 @@ export default {
   .Add-stylus{
     background-color: rgb(130, 211, 222);
     padding: 10px;
-    border-radius: 9px;
+    border-radius: 10px;
     color: white;
     font-weight: bold;
   }
@@ -86,7 +121,7 @@ export default {
       padding: 8px;
       color: white;
       margin: 3px;
-      border-radius: 9px;
+      border-radius: 10px;
     }
     &.mainbutton{
       background-color: rgb(130, 211, 222);
@@ -94,12 +129,15 @@ export default {
       padding: 8px;
       color: white;
       margin: 3px;
-      border-radius: 9px;
+      border-radius: 10px;
     }
   }
   
   table{
     width: 100%;
+    & thead tr{
+      height: 70px;
+    }
   }
   tbody{
     & tr{
@@ -115,7 +153,33 @@ export default {
     }
   }
   
+  .pagination_container{
+    text-align: center;
+    margin: 10px;
+    & .page-item.disabled{
+      color: gray;
+    }
+    & .page-item.page-number.active{
+      color: #ffffff;
+      & a.page-link{
+        background-color:rgb(130, 211, 222);
+        border-radius: 10px
+      }
+    }
+  }
+  
    div.head{
-    min-height: 60px;
+    min-height: 38px;
+  }
+
+  .filters{
+    display: flex;
+    float: left;
+    margin: 10px;
+    & input{
+      border-radius: 10px;
+      height: 40px;
+      font-size: 18px;
+    }
   }
 </style>
